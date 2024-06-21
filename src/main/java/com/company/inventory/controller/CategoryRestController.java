fc44e2f9e18221228140d0d80a1b3edee2262b61
Ana Maria Condori <anacondori@hotmail.com>
2024-06-21 12:22:04 +0200

    category

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -2,6 +2,7 @@ package com.company.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.company.inventory.response.CategoryResponseRest;
 import com.company.inventory.services.ICategoryService;
 
 //punto de entrada para q cualquier aplicacion externa lo pueda consumir
+@CrossOrigin(origins = {"http://localhost:4200"} )
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
